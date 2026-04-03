854c98f19fdbb0f8258f44730f8bdc17225b4a75
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-03 14:23:23 +0530

    feat: add logger initialization and status constants for success/failure handling

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,61 @@
 import { StatusCodes } from 'http-status-codes';
 import pino from 'pino';
 import userService from '../services/user.service.js';
+
+const logger = pino();
+
+const STATUS = {
+success: true,
+failure: false
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
