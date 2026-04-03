9fcd6f1ffe237555ca90c6d06cf6ca6a2e776df2
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-03 14:36:43 +0530

    defined sampleUser before each test for consistent test data

--- src/services/__tests__/user.service.test.js
+++ src/services/__tests__/user.service.test.js
@@ -1,3 +1,46 @@
 import test from 'ava';
 
 import userService from "../user.service";
+
+
+let sampleUser;
+
+test.beforeEach(() => {
+    sampleUser = {
+        name: 'Joe Doe',
+        email: 'joedoe@email.com',
+        city: 'New York',
+        country: 'USA'
+    };
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
