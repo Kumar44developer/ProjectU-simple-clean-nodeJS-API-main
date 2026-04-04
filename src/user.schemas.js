217511679f2e5af925e812bceb7b4235374795a2
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-04 12:32:43 +0530

    completed addUser schema with city and country validation

--- src/user.schemas.js
+++ src/user.schemas.js
@@ -33,7 +33,12 @@ export const addUser = {
             yupSchema: yup.object().shape({
                 name: yup.string().min(MIN_LENGTH.name).max(MAX_LENGTH.name),
                 email: yup.string().email().max(MAX_LENGTH.email),
-
+                city: yup.string().min(MIN_LENGTH.city).max(MAX_LENGTH.city),
+                country: yup.string().min(MIN_LENGTH.country).max(MAX_LENGTH.country),
+            })
+        },
+    },
+};
 
 
 
