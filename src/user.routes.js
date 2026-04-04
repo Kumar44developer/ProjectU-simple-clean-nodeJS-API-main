41c2334c51c88beef39a98667551416ddddc7285
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-04 12:27:52 +0530

    imported user controller and user schema validators

--- src/user.routes.js
+++ src/user.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { expressYupMiddleware } from 'express-yup-middleware';
 
-
+import userController from './controllers/user.controller.js';
+import {getUser, addUser, updateUser, removeUser} from './user.schemas';
 
 
 
